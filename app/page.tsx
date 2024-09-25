73d1b386f32bdc865fa7412c1eacfaf5ae1b382b
agamya0801 <vrishabh.agamya@theporter.in>
2024-09-25 09:08:38 +0530

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Hero from "@/components/hero";
+
+export default function Home() {
+  return (
+    <main className="relative bg-black-100">
+      <Hero />
+    </main>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,36 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        black: {
+          DEFAULT: '#000',
+          100: '#000319'
+        }
+      },
+      animation: {
+        spotlight: "spotlight 2s ease .75s 1 forwards",
+      },
+      keyframes: {
+        spotlight: {
+          "0%": {
+            opacity: '0',
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: '1',
+            transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
